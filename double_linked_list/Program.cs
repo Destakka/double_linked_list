7fc9241a3c9b0b15bef552fbc8981f4d6f75f56a
DESTA ATIKANANDA <d.atikananda.ft21@mail.umy.ac.id>
2022-11-18 17:20:00 +0700

    add variabel node previous, current

--- double_linked_list/Program.cs
+++ double_linked_list/Program.cs
@@ -48,6 +48,25 @@ namespace double_linked_list
                 START = newNode;
                 return;
             }
+            /*if the node is to be inserted at between two Node*/
+            Node previous, current;
+            for (current = previous = START;
+                current != null && nim >= current.noMhs;
+                previous = current, current = current.next)
+            {
+                if (nim == current.noMhs)
+                {
+                    Console.WriteLine("\n Duplicate roll numbers not allowed");
+                }
+            }
+
+            /*On the execution of the above for loop, prev and
+             * Current will point to those nodes
+             * between which the new node is to be inserted*/
+            newNode.next = current;
+            newNode.prev = previous;
+
+            //if
         }
     }
     class Program
