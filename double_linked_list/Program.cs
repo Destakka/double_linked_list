87f25ec21c9407af8f41830ada8859b12512186d
DESTA ATIKANANDA <d.atikananda.ft21@mail.umy.ac.id>
2022-11-18 17:28:42 +0700

    add public bool Search

--- double_linked_list/Program.cs
+++ double_linked_list/Program.cs
@@ -66,7 +66,29 @@ namespace double_linked_list
             newNode.next = current;
             newNode.prev = previous;
 
-            //if
+            //if the node is to be inserted at the end of the list
+            if (current == null)
+            {
+                newNode.next = null;
+                previous.next = newNode;
+                return;
+            }
+
+            current.prev = newNode;
+            previous.next = newNode;
+        }
+
+        public bool Search(int rollNo, ref Node previous, ref Node current)
+        {
+            previous = current = START;
+            while (current != null &&
+                rollNo != current.noMhs)
+            {
+                previous = current;
+                current = current.next;
+            }
+            return ( current != null );
+
         }
     }
     class Program
