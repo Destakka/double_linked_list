5a0cf74a830465cdd02b906d8e5180a0f65a342b
DESTA ATIKANANDA <d.atikananda.ft21@mail.umy.ac.id>
2022-11-18 16:54:13 +0700

    Add project files.

---
+++ double_linked_list/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace double_linked_list
+{
+    class Node
+    {
+        /*Node class represent the node of dpubly linked list
+         *It consist of the information part and links to
+         *Its succeding and preeceding
+         *In terms of next and previous */
+        public int noMhs;
+        public string name;
+        //point to the succeding node
+        public Node next;
+        //point to the preceding node
+        public Node prev;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
